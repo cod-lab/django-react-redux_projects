1ec5cd9528d5e1f48c82657cb242076b9f92e7fa
cod-lab <arihant806@gmail.com>
2020-06-18 00:09:10 +0530

    login functionality added to app 'frontend'

---
+++ 1_lead-manager/.history/leadmanager/frontend/src/actions/types_20200616174454.js
@@ -0,0 +1,11 @@
+// its just a place to hold all of the types of actions that will be performed in a website
+export const GET_LEADS = "GET_LEADS";
+export const DELETE_LEAD = "DELETE_LEAD";
+export const ADD_LEAD = "ADD_LEAD";
+export const GET_ERRORS = "GET_ERRORS";
+export const CREATE_MESSAGE = "CREATE_MESSAGE";
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";           // if authenticated then its true 
+export const AUTH_ERROR = "AUTH_ERROR";             // if not authenticated then its true 
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";             // if not authenticated then its true 
+export const LOGIN_FAIL = "LOGIN_FAIL";             // if not authenticated then its true 
---
+++ 1_lead-manager/.history/leadmanager/frontend/src/components/accounts/Login_20200617225614.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';                                      
+// 'connect' is used to work with 'redux' from any 'component'(current file)
+// 'connect' is used to interact 'component' with 'redux'
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';                                         // we call 'login' when 'component' mounts (basically when calling fn 'componentDidMount')
+
+export class Login extends Component {
+
+    state = {
+    // adding state for each input
+    // it is a componnent level state, it has nothing to do with Redux
+        username: '',
+        password: ''
+    };
+
+    static propTypes = {
+        login: PropTypes.func.isRequired,                                           // creating a 'prop' 'login'
+        isAuthenticated: PropTypes.bool                                             // creating a 'prop' 'isAuthenticated'
+    };
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });             // creating event 'e', setting component level state
+
+    onSubmit = e => {
+        e.preventDefault();
+        this.props.login(this.state.username, this.state.password);
+    };
+
+    render() {
+        if (this.props.isAuthenticated)
+            return <Redirect to='/' />;
+
+        const { username, password } = this.state;                                  // getting data for 'from-value' from above state
+
+        return (
+            <div classNam="col-md-6 m-auto">
+                <div classNam="card card-body mt-5">
+                    <h2 className="text-center">Login</h2>
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input className="form-control" type="text" name="username" onChange={this.onChange} value={username} />
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input className="form-control" type="password" name="password" onChange={this.onChange} value={password} />
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary" type="submit">Login</button>
+                        </div>
+                        <p>Don't have an account? <Link to="/register">Register</Link></p> 
+                        {/* 'Link' imported from react-route-dom */}
+                        {/* 'Link' will add the given string to url by replacing the string succeeding default or home url and run the resultant url*/}
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({ isAuthenticated: state.auth.isAuthenticated });
+
+export default connect(mapStateToProps, { login })(Login);
